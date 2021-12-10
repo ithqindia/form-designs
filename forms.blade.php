f29d4332dd198f60a9fe08b15e34e5e740d0d4c1
Kajal Manohar Nimje <kajalnimje1998@gamail.com>
2021-12-10 11:25:10 +0530

    code updated

--- forms.blade.php
+++ forms.blade.php
@@ -3,36 +3,35 @@
 @section('container')
 <section>
     {{--  permananat address form --}}
-    <div class="container permanant_address">
+    <div class="container Permanent_address">
         <form class="row g-3 ">
-            <h4> Address(permanant)</h4>
+            <h4> Permanent Address</h4>
             <div class="col-12">
                 <label for="address" class="form-label">Address</label>
-                <input type="text" class="form-control" id="permanant_address" name="permanant_address">
+                <input type="text" class="form-control" id="permanent_address" name="permanaet_address">
             </div>
             <div class="col-12">
-                <label for="apt" class="form-label">Apt/Suit/etc</label>
-                <input type="text" class="form-control" id="permanant_apt" name="permanant_apt">
+                <label for="apt" class="form-label">Apartment, Suite, etc</label>
+                <input type="text" class="form-control" id="permanent_apartment" name="permanent_apt">
             </div>
             <div class="col-12">
-                <label for="town" class="form-label">City/Town</label>
-                <input type="text" class="form-control" id="permanant_town" name="permanant_town" required>
+                <label for="city" class="form-label">City/Town</label>
+                <input type="text" class="form-control" id="permanent_city" name="permanent_city" required>
             </div>
             <div class="col-12">
                 <label for="pin" class="form-label">Pincode</label>
-                <input type="number" class="form-control" id="permanant_pin" name="permanant_pin"
-                    placeholder="****" required>
+                <input type="number" class="form-control" id="permanent_pin" name="permanent_pin" required>
             </div>
             <div class="col-12">
                 <label for="state" class="form-label">State</label>
-                <input type="text" class="form-control" id="permanant_state" name="permanant_state" required>
+                <input type="text" class="form-control" id="permanent_state" name="permanent_state" required>
             </div>
             <div class="col-12">
                 <label for="country" class="form-label">country</label>
-                <input type="text" class="form-control" id="permanant_country" name="permanant_country" required>
+                <input type="text" class="form-control" id="permanent_country" name="permanent_country" required>
             </div>
             <div class="col-12">
-                <button type="submit" class="btn btn-primary" id=permanant_submit> Submit</button>
+                <button type="submit" class="btn btn-primary" id=permanent_submit> Submit</button>
             </div>
         </form>
     </div>
@@ -72,8 +71,7 @@
             </div>
             <div class="col-12">
                 <label for="pin" class="form-label">Pincode</label>
-                <input type="number" class="form-control" id="local_pin" name="local_pin" placeholder="****"
-                    required>
+                <input type="number" class="form-control" id="local_pin" name="local_pin" required>
             </div>
             <div class="col-12">
                 <label for="state" class="form-label">State</label>
