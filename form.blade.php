d66cdf01c0045dd7f89ce6d473083bddd58ec7a1
sumedh-mohod <sumedhmohod72@gmail.com>
2021-12-10 11:45:33 +0530

    code updated

--- form.blade.php
+++ form.blade.php
@@ -6,7 +6,7 @@
     <div class="">
     <div class="accordion-item">
       <h2 class="accordion-header" id="headingone_ssc">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#ssc_details" aria-expanded="true" aria-controls="collapseOne">
           10th
         </button>
       </h2>
@@ -15,23 +15,23 @@
             <form class="row g-3">
                 <div class="col-md-12">
                   <label for="inputCollege" class="form-label">School</label>
-                  <input type="text" class="form-control" id="inputCollege_ssc" name="school">
+                  <input type="text" class="form-control" id="inputCollege_ssc" name="ssc_school">
                 </div>
                 <div class="col-md-12">
                   <label for="inputUniversity" class="form-label">State Board</label>
-                  <input type="text" class="form-control" id="inputUniversity_ssc" name="sscBoard">
+                  <input type="text" class="form-control" id="inputUniversity_ssc" name="ssc_board">
                 </div>
                 <div class="col-12">
                   <label for="inputAggregates" class="form-label">Percentage</label>
-                  <input type="text" class="form-control" id="inputAggregates_ssc" placeholder="Aggregates" name="sscPercentage">
+                  <input type="text" class="form-control" id="inputAggregates_ssc" placeholder="Aggregates" name="ssc_percentage">
                 </div>
                 <div class="col-12">
                     <label for="inputYearfrom" class="form-label">Year Form</label>
-                    <input type="text" class="form-control" id="inputYearfrom_ssc" placeholder="Year Form" name="schoolYearform">
+                    <input type="text" class="form-control" id="inputYearfrom_ssc" placeholder="Year Form" name="school_yearform">
                   </div>
                   <div class="col-12">
                     <label for="inputYearto" class="form-label">Year To</label>
-                    <input type="text" class="form-control" id="inputYearto_ssc" placeholder="Year To" name="schoolYearto">
+                    <input type="text" class="form-control" id="inputYearto_ssc" placeholder="Year To" name="school_yearto">
                   </div>
                 <div class="col-12">
                   <button type="submit" class="btn btn-primary">Submit</button>
@@ -46,7 +46,7 @@
     <div class = "">
     <div class="accordion-item">
       <h2 class="accordion-header" id="headingTwo_hsc">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#hsc_details" aria-expanded="false" aria-controls="collapseTwo">
           12th
         </button>
       </h2>
@@ -55,23 +55,23 @@
             <form class="row g-3">
                 <div class="col-md-12">
                   <label for="inputCollege" class="form-label">College</label>
-                  <input type="text" class="form-control" id="inputCollege_hsc" name="hscCollege">
+                  <input type="text" class="form-control" id="inputCollege_hsc" name="hsc_college">
                 </div>
                 <div class="col-md-12">
                   <label for="inputUniversity" class="form-label">HSC Board</label>
-                  <input type="text" class="form-control" id="inputUniversity_hsc" name="hscBoard">
+                  <input type="text" class="form-control" id="inputUniversity_hsc" name="hsc_board">
                 </div>
                 <div class="col-12">
                   <label for="inputAggregates" class="form-label">Percentage</label>
-                  <input type="text" class="form-control" id="inputAggregates_hsc" placeholder="Percentage" name="hscPercentage">
+                  <input type="text" class="form-control" id="inputAggregates_hsc" placeholder="Percentage" name="hsc_percentage">
                 </div>
                 <div class="col-12">
                     <label for="inputYearfrom" class="form-label">Year Form</label>
-                    <input type="text" class="form-control" id="inputYearfrom_hsc" placeholder="Year Form" name="hscYearform">
+                    <input type="text" class="form-control" id="inputYearfrom_hsc" placeholder="Year Form" name="hsc_yearform">
                   </div>
                   <div class="col-12">
                     <label for="inputYearto" class="form-label">Year To</label>
-                    <input type="text" class="form-control" id="inputYearto_hsc" placeholder="Year To" name="hscYearto">
+                    <input type="text" class="form-control" id="inputYearto_hsc" placeholder="Year To" name="hsc_yearto">
                   </div>
                 <div class="col-12">
                   <button type="submit" class="btn btn-primary">Submit</button>
@@ -86,7 +86,7 @@
     <div class = "">
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingOne_graduation">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#graduation_details" aria-expanded="true" aria-controls="collapseOne">
             Graduation
           </button>
         </h2>
@@ -95,23 +95,23 @@
               <form class="row g-3">
                   <div class="col-md-12">
                     <label for="inputCollege" class="form-label">College</label>
-                    <input type="text" class="form-control" id="inputCollege_graduation" name="graduationCollege">
+                    <input type="text" class="form-control" id="inputCollege_graduation" name="graduation_college">
                   </div>
                   <div class="col-md-12">
                     <label for="inputUniversity" class="form-label">University</label>
-                    <input type="text" class="form-control" id="inputUniversity_graduation" name="graduationUniversity">
+                    <input type="text" class="form-control" id="inputUniversity_graduation" name="graduation_university">
                   </div>
                   <div class="col-12">
                     <label for="inputAggregates" class="form-label">Aggregates</label>
-                    <input type="text" class="form-control" id="inputAggregates_graduation" placeholder="Aggregates" name="graduationAggregates">
+                    <input type="text" class="form-control" id="inputAggregates_graduation" placeholder="Aggregates" name="graduation_aggregates">
                   </div>
                   <div class="col-12">
                       <label for="inputYearfrom" class="form-label">Year Form</label>
-                      <input type="text" class="form-control" id="inputYearfrom_graduation" placeholder="Year Form" name="graduationYearfrom">
+                      <input type="text" class="form-control" id="inputYearfrom_graduation" placeholder="Year Form" name="graduation_yearfrom">
                     </div>
                     <div class="col-12">
                       <label for="inputYearto" class="form-label">Year To</label>
-                      <input type="text" class="form-control" id="inputYearto_graduation" placeholder="Year To" name="graduationYearto">
+                      <input type="text" class="form-control" id="inputYearto_graduation" placeholder="Year To" name="graduation_yearto">
                     </div>
                   <div class="col-12">
                     <button type="submit" class="btn btn-primary">Submit</button>
@@ -125,7 +125,7 @@
     <div class="">
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingOne_diploma">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#diploma_details" aria-expanded="true" aria-controls="collapseOne">
             Diploma
           </button>
         </h2>
@@ -134,23 +134,23 @@
               <form class="row g-3">
                   <div class="col-md-12">
                     <label for="inputCollege" class="form-label">College</label>
-                    <input type="text" class="form-control" id="inputCollege_diploma" name="diplomaCollege">
+                    <input type="text" class="form-control" id="inputCollege_diploma" name="diploma_college">
                   </div>
                   <div class="col-md-12">
                     <label for="inputUniversity" class="form-label">University</label>
-                    <input type="text" class="form-control" id="inputUniversity_diploma" name="diplomaUniversity">
+                    <input type="text" class="form-control" id="inputUniversity_diploma" name="diploma_university">
                   </div>
                   <div class="col-12">
                     <label for="inputAggregates" class="form-label">Aggregates</label>
-                    <input type="text" class="form-control" id="inputAggregates_diploma" placeholder="Aggregates" name="diplomaAggregates">
+                    <input type="text" class="form-control" id="inputAggregates_diploma" placeholder="Aggregates" name="diploma_aggregates">
                   </div>
                   <div class="col-12">
                       <label for="inputYearfrom" class="form-label">Year Form</label>
-                      <input type="text" class="form-control" id="inputYearfrom_diploma" placeholder="Year Form" name="diplomaYearform">
+                      <input type="text" class="form-control" id="inputYearfrom_diploma" placeholder="Year Form" name="diploma_yearform">
                     </div>
                     <div class="col-12">
                       <label for="inputYearto" class="form-label">Year To</label>
-                      <input type="text" class="form-control" id="inputYearto_diploma" placeholder="Year To" name="diplomaYearto">
+                      <input type="text" class="form-control" id="inputYearto_diploma" placeholder="Year To" name="diploma_yearto">
                     </div>
                   <div class="col-12">
                     <button type="submit" class="btn btn-primary">Submit</button>
@@ -165,7 +165,7 @@
     <div clas="">
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingOne_postGraduation">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#postgraduation_details" aria-expanded="true" aria-controls="collapseOne">
             Post Graduation
           </button>
         </h2>
@@ -174,23 +174,23 @@
               <form class="row g-3">
                   <div class="col-md-12">
                     <label for="inputCollege" class="form-label">College</label>
-                    <input type="text" class="form-control" id="inputCollege_postGraduation" name="postgraduationCollege">
+                    <input type="text" class="form-control" id="inputCollege_postGraduation" name="postgraduation_college">
                   </div>
                   <div class="col-md-12">
                     <label for="inputUniversity" class="form-label">University</label>
-                    <input type="text" class="form-control" id="inputUniversity_postGraduation" name="postgraduationUniversity">
+                    <input type="text" class="form-control" id="inputUniversity_postGraduation" name="postgraduation_university">
                   </div>
                   <div class="col-12">
                     <label for="inputAggregates" class="form-label">Aggregates</label>
-                    <input type="text" class="form-control" id="inputAggregates_postGraduation" placeholder="Aggregates" name="postgraduationAggregates">
+                    <input type="text" class="form-control" id="inputAggregates_postGraduation" placeholder="Aggregates" name="postgraduation_aggregates">
                   </div>
                   <div class="col-12">
                       <label for="inputYearfrom" class="form-label">Year Form</label>
-                      <input type="text" class="form-control" id="inputYearfrom_postGraduation" placeholder="Year Form" name="postgraduationYearfrom">
+                      <input type="text" class="form-control" id="inputYearfrom_postGraduation" placeholder="Year Form" name="postgraduation_yearfrom">
                     </div>
                     <div class="col-12">
                       <label for="inputYearto" class="form-label">Year To</label>
-                      <input type="text" class="form-control" id="inputYearto_postGraduation" placeholder="Year To" name="postgraduationYearto">
+                      <input type="text" class="form-control" id="inputYearto_postGraduation" placeholder="Year To" name="postgraduation_yearto">
                     </div>
                   <div class="col-12">
                     <button type="submit" class="btn btn-primary">Submit</button>
